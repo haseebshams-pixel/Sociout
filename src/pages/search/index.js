9d8b4b76601a5d11867e7621233057706733a3e4
unknown <ali.naveed7305@gmail.com>
2022-04-20 14:51:59 +0500

    search screen done

--- src/pages/search/index.js
+++ src/pages/search/index.js
@@ -1,9 +1,31 @@
 import React from "react";
+import PersonCard from "../../shared/components/common/personCard";
+import "./style.css";
 
 const Search = () => {
   return (
-    <div>
-      <div>Search</div>
+    <div className="container">
+      <div className="text-center search_searchinput">
+        <input
+          type="text"
+          className="w-100 mt-4 mb-4 ps-3 pt-2 pb-2 pe-5 rounded search_inputag"
+          placeholder="Search..."
+        ></input>
+        <span className="search_iconinside">
+          <img
+            src={require("../../assets/svg/search.svg").default}
+            width="22"
+            height="22"
+          />
+        </span>
+      </div>
+      <div className="d-flex flex-wrap mx-100 justify-content-center">
+        <PersonCard />
+        <PersonCard />
+        <PersonCard />
+        <PersonCard />
+        <PersonCard />
+      </div>
     </div>
   );
 };
