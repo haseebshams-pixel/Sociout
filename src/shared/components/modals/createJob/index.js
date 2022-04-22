8a2c948c185f232a337b8bf1b272496ece792454
Mabdullahshahzad <abdullahshahzad.pk.lhr@gmail.com>
2022-04-22 10:02:10 -0700

    Post Job Added

--- src/shared/components/modals/createJob/index.js
+++ src/shared/components/modals/createJob/index.js
@@ -1,9 +1,30 @@
-import React from "react";
-import FeatherIcon from "feather-icons-react";
-import { Modal } from "react-bootstrap";
-import "./style.css";
+import React from 'react'
+import FeatherIcon from 'feather-icons-react'
+import { Modal } from 'react-bootstrap'
+import './style.css'
+import { CreateJobVS } from '../../../utils/validation'
+import { Formik } from 'formik'
+import { Spinner } from 'react-bootstrap'
+import Form from 'react-bootstrap/Form'
+import FloatingLabel from 'react-bootstrap/FloatingLabel'
+import { toastMessage } from '../../common/toast'
 
 const CreateJobModal = ({ show, hide }) => {
+  const initialValues = {
+    jobTitle: '',
+    companyName: '',
+    employmentType: '',
+    location: '',
+    contact: '',
+    description: '',
+  }
+
+  const handleChangePass = async (values, action) => {
+    action.setSubmitting(false)
+    hide()
+    toastMessage('Password Updated Successfully', 'success')
+  }
+
   return (
     <Modal
       show={show}
@@ -26,10 +47,141 @@ const CreateJobModal = ({ show, hide }) => {
           </div>
         </div>
         <hr className="m-0 mb-3 mt-2" />
-        <Modal.Body className="d-flex flex-column p-0 pb-3"></Modal.Body>
+        <Modal.Body className="d-flex flex-column p-0 pb-3">
+          <Formik
+            initialValues={initialValues}
+            onSubmit={(values, action) => {
+              action.setSubmitting(true)
+              handleChangePass(values, action)
+            }}
+            validationSchema={CreateJobVS}
+          >
+            {({
+              values,
+              errors,
+              touched,
+              handleChange,
+              handleSubmit,
+              isSubmitting,
+            }) => (
+              <div className="mt-3">
+                <div className="position-relative">
+                  <FloatingLabel controlId="floatingPassword" label="Job Title">
+                    <Form.Control
+                      type="text"
+                      placeholder="Job Title"
+                      onChange={handleChange('jobTitle')}
+                      value={values.jobTitle}
+                      className="mb-4"
+                    />
+                  </FloatingLabel>
+                  <div className="error">
+                    {touched.jobTitle && errors.jobTitle ? errors.jobTitle : ''}
+                  </div>
+                </div>
+                <div className="position-relative">
+                  <FloatingLabel
+                    controlId="floatingPassword"
+                    label="Company Name"
+                  >
+                    <Form.Control
+                      type="text"
+                      placeholder="Company Name"
+                      onChange={handleChange('companyName')}
+                      value={values.companyName}
+                      className="mb-4"
+                    />
+                  </FloatingLabel>
+                  <div className="error">
+                    {touched.companyName && errors.companyName
+                      ? errors.companyName
+                      : ''}
+                  </div>
+                </div>
+                <div className="position-relative">
+                  <FloatingLabel
+                    controlId="floatingPassword"
+                    label="Employment Type"
+                  >
+                    <Form.Control
+                      type="text"
+                      placeholder="Employment Type"
+                      onChange={handleChange('employmentType')}
+                      value={values.employmentType}
+                      className="mb-4"
+                    />
+                  </FloatingLabel>
+                  <div className="error">
+                    {touched.employmentType && errors.employmentType
+                      ? errors.employmentType
+                      : ''}
+                  </div>
+                </div>
+                <div className="position-relative">
+                  <FloatingLabel controlId="floatingPassword" label="Location">
+                    <Form.Control
+                      type="text"
+                      placeholder="Location"
+                      onChange={handleChange('location')}
+                      value={values.location}
+                      className="mb-4"
+                    />
+                  </FloatingLabel>
+                  <div className="error">
+                    {touched.location && errors.location ? errors.location : ''}
+                  </div>
+                </div>
+                <div className="position-relative">
+                  <FloatingLabel controlId="floatingPassword" label="Contact">
+                    <Form.Control
+                      type="email"
+                      placeholder="name@example.com"
+                      onChange={handleChange('contact')}
+                      value={values.contact}
+                      className="mb-4"
+                    />
+                  </FloatingLabel>
+                  <div className="error">
+                    {touched.contact && errors.contact ? errors.contact : ''}
+                  </div>
+                </div>
+                <div className="position-relative">
+                  <FloatingLabel
+                    controlId="floatingPassword"
+                    label="Description"
+                  >
+                    <textarea
+                      className="form-control mb-4"
+                      rows="5"
+                      placeholder="Description"
+                      onChange={handleChange('description')}
+                      value={values.description}
+                    ></textarea>
+                  </FloatingLabel>
+                  <div className="error">
+                    {touched.description && errors.description
+                      ? errors.description
+                      : ''}
+                  </div>
+                </div>
+
+                <button
+                  className="w-100 btn login-btn mt-2"
+                  onClick={handleSubmit}
+                >
+                  {isSubmitting ? (
+                    <Spinner animation="grow" size="sm" />
+                  ) : (
+                    <p className="mb-0 login-btn-txt-size">Post</p>
+                  )}
+                </button>
+              </div>
+            )}
+          </Formik>
+        </Modal.Body>
       </div>
     </Modal>
-  );
-};
+  )
+}
 
-export default CreateJobModal;
+export default CreateJobModal
--- src/shared/utils/validation.js
+++ src/shared/utils/validation.js
@@ -1,67 +1,89 @@
-import * as yup from "yup";
+import * as yup from 'yup'
 
 const LoginVS = yup.object().shape({
   email: yup
     .string()
-    .required("Email is Required")
-    .email("Invalid Email")
-    .label("email"),
-  password: yup.string().required("Password is Required").label("password"),
-});
+    .required('Email is Required')
+    .email('Invalid Email')
+    .label('email'),
+  password: yup.string().required('Password is Required').label('password'),
+})
 
 const RegistrationVS = yup.object().shape({
   email: yup
     .string()
-    .required("Email is Required")
-    .email("Invalid Email")
-    .label("email"),
-  firstName: yup.string().required("FirstName is Required").label("firstName"),
-  lastName: yup.string().required("LastName is Required").label("lastName"),
+    .required('Email is Required')
+    .email('Invalid Email')
+    .label('email'),
+  firstName: yup.string().required('FirstName is Required').label('firstName'),
+  lastName: yup.string().required('LastName is Required').label('lastName'),
   phoneNumber: yup
     .string()
-    .required("Phone Number is Required")
-    .label("phonenumber"),
-  password: yup.string().required("Password is Required"),
+    .required('Phone Number is Required')
+    .label('phonenumber'),
+  password: yup.string().required('Password is Required'),
   confirmPassword: yup
     .string()
-    .required("Confirm Password is Required")
-    .oneOf([yup.ref("password"), null], "Passwords must match"),
+    .required('Confirm Password is Required')
+    .oneOf([yup.ref('password'), null], 'Passwords must match'),
 
-  DOB: yup.string().required("Date of Birth is Required").label("DOB"),
-});
+  DOB: yup.string().required('Date of Birth is Required').label('DOB'),
+})
 
 const ChangePasswordVS = yup.object().shape({
   oldPassword: yup
     .string()
-    .required("Old Password is Required")
-    .label("oldPassword"),
+    .required('Old Password is Required')
+    .label('oldPassword'),
   newPassword: yup
     .string()
-    .required("New Password is Required")
-    .label("newPassword"),
+    .required('New Password is Required')
+    .label('newPassword'),
   confirmPassword: yup
     .string()
-    .required("Confirm Password is Required")
-    .oneOf([yup.ref("newPassword"), null], "Passwords must match"),
-});
+    .required('Confirm Password is Required')
+    .oneOf([yup.ref('newPassword'), null], 'Passwords must match'),
+})
 
 const EditProfileVS = yup.object().shape({
-  firstname: yup.string().required("Firstname is Required").label("firstname"),
-  lastname: yup.string().required("Lastname is Required").label("lastname"),
+  firstname: yup.string().required('Firstname is Required').label('firstname'),
+  lastname: yup.string().required('Lastname is Required').label('lastname'),
   phoneNumber: yup
     .string()
-    .required("Phone Number is Required")
-    .label("phonenumber"),
-  DOB: yup.string().required("Date of Birth is Required").label("DOB"),
-});
+    .required('Phone Number is Required')
+    .label('phonenumber'),
+  DOB: yup.string().required('Date of Birth is Required').label('DOB'),
+})
 
 const ResetPasswordVS = yup.object().shape({
   email: yup
     .string()
-    .required("Email is Required")
-    .email("Invalid Email")
-    .label("email"),
-});
+    .required('Email is Required')
+    .email('Invalid Email')
+    .label('email'),
+})
+
+const CreateJobVS = yup.object().shape({
+  jobTitle: yup.string().required('Job Title is Required').label('jobTitle'),
+  companyName: yup
+    .string()
+    .required('CompanyName is Required')
+    .label('companyName'),
+  employmentType: yup
+    .string()
+    .required('EmploymentType is Required')
+    .label('employmentType'),
+  location: yup.string().required('Location is Required').label('location'),
+  contact: yup
+    .string()
+    .required('Contact is Required')
+    .email('Invalid Email')
+    .label('contact'),
+  description: yup
+    .string()
+    .required('Description is Required')
+    .label('description'),
+})
 
 export {
   LoginVS,
@@ -69,4 +91,5 @@ export {
   ChangePasswordVS,
   EditProfileVS,
   ResetPasswordVS,
-};
+  CreateJobVS,
+}
