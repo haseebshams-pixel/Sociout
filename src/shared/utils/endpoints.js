250f6cebffb52e466965443c663e5f6e7b734c47
haseebshams-pixel <hadishams38.hs@gmail.com>
2023-01-04 10:13:00 +0500

    chnged base url to live server

--- src/shared/utils/endpoints.js
+++ src/shared/utils/endpoints.js
@@ -1,5 +1,5 @@
-// const BaseURL = "https://sociout-dev.fly.dev/api/";
-const BaseURL = "http://localhost:8000/api/";
+const BaseURL = "https://sociout-dev.fly.dev/api/";
+// const BaseURL = "http://localhost:8000/api/";
 
 const PhotoURL = "https://storage.googleapis.com/sociout-data/";
 
